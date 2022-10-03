b499d849b3040602c8cf55a77d88cf2853cbd235
noman62 <noman.ice.nstu@gmail.com>
2022-10-03 06:11:42 -0700

    change

--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -45,62 +45,71 @@ const Form = () => {
       <div style={{ marginLeft: '200px' }}>
         <div className='row'>
           <div id='upload' className='  col-8'>
-            <form className='upload-form' onSubmit={handleSubmit}>
-              <div className='form-row'>
-                <div className='form-group col-md-12'>
-                  <label for='notice-number'>Product name</label>
-                  <input
-                    type='text'
-                    className='form-control form-control-sm'
-                    id='notice-number'
-                    required
-                    placeholder='Type Product name'
-                    onChange={handleChange}
-                    name='name'
-                    //   onChange={handleChange}
-                    defaultValue=''
-                  />
-                </div>
-              </div>
-              <div className='form-row'>
-                <div className='form-group col-md-12'>
-                  <label for='short-title'>NID Number</label>
-                  <input
-                    type='number'
-                    className='form-control form-control-sm'
-                    id='short-title'
-                    required
-                    placeholder='Type NID Number'
-                    onChange={handleChange}
-                    name='nid'
-                    //   onChange={handleChange}
-                    defaultValue=''
-                  />
-                </div>
-              </div>
+          {user === null && (
+            <>
+            window.alert("please login")
+            </>
+          )}
+            {user !== null && user.user && (
+              <>
+                <form className='upload-form' onSubmit={handleSubmit}>
+                  <div className='form-row'>
+                    <div className='form-group col-md-12'>
+                      <label for='notice-number'>Product name</label>
+                      <input
+                        type='text'
+                        className='form-control form-control-sm'
+                        id='notice-number'
+                        required
+                        placeholder='Type Product name'
+                        onChange={handleChange}
+                        name='name'
+                        //   onChange={handleChange}
+                        defaultValue=''
+                      />
+                    </div>
+                  </div>
+                  <div className='form-row'>
+                    <div className='form-group col-md-12'>
+                      <label for='short-title'>NID Number</label>
+                      <input
+                        type='number'
+                        className='form-control form-control-sm'
+                        id='short-title'
+                        required
+                        placeholder='Type NID Number'
+                        onChange={handleChange}
+                        name='nid'
+                        //   onChange={handleChange}
+                        defaultValue=''
+                      />
+                    </div>
+                  </div>
 
-              <div class='form-row'>
-                <div class='form-group col-md-12'>
-                  <label for='description'>Reasons</label>
-                  <textarea
-                    class='form-control form-control-sm'
-                    id='exampleFormControlTextarea1'
-                    rows='2'
-                    placeholder='short Description why you need it'
-                    name='reasons'
-                    onChange={handleChange}
-                  ></textarea>
-                </div>
-              </div>
+                  <div class='form-row'>
+                    <div class='form-group col-md-12'>
+                      <label for='description'>Reasons</label>
+                      <textarea
+                        class='form-control form-control-sm'
+                        id='exampleFormControlTextarea1'
+                        rows='2'
+                        placeholder='short Description why you need it'
+                        name='reasons'
+                        onChange={handleChange}
+                      ></textarea>
+                    </div>
+                  </div>
 
-              <div className='form-row'>
-                <div className='form-group col-md-12 text-center'>
-                  <button type='submit' className=' btn btn-info w-50'>
-                    Submit{' '}
-                  </button>
-                </div>
-              </div>
-            </form>
+                  <div className='form-row'>
+                    <div className='form-group col-md-12 text-center'>
+                      <button type='submit' className=' btn btn-info w-50'>
+                        Submit{' '}
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </>
+            )}
           </div>
         </div>
       </div>
