ed93a9346b1ae100d66cdbd8b4252e8f5f488795
noman62 <noman.ice.nstu@gmail.com>
2022-10-09 10:28:53 -0700

    add loading

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,6 +13,7 @@ const Header = () => {
     // make state null
     const { data } = await axios.get('https://immense-badlands-43010.herokuapp.com/api/logout');
     dispatch(logout());
+    window.alert("logout Successfully")
     history.push("/login");
     
   };
